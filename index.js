24fdb3e1fe35552d5175cf985e9b83eece624827
srabon0 <srabonemam4@gmail.com>
2022-10-06 12:27:36 +0600

    final deploy commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const userRouter = require('./routes/routes.user')
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5000;
 app.use(express.json())
 app.get('/', (req, res) => {
   res.send('Hello World!');
